d8f242a80ed9500f0f7aba372c12362808565fb4
Nastya-Lopatina <nastya.lopatina.93@gmail.com>
2021-04-19 14:06:16 +0900

    ДЗ готово

--- homework-gallery.js
+++ homework-gallery.js
@@ -6,37 +6,63 @@ const backdropEl = document.querySelector('.lightbox__overlay')
 const openModalBigImg = document.querySelector('.lightbox__image');
 const btnEl = document.querySelector('button[data-action="close-lightbox"]');
 
-const cardsImages = createGalleryLayout(pictureFile);
-
-galleryListEl.insertAdjacentHTML('beforeend', cardsImages);
-
 function createGalleryLayout(images) {
 return images.map(({ preview, original, description }) => {
 return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+  <a class="gallery__link"
+  href="${original}">
+    <img class="gallery__image"
+    src="${preview}" 
+    data-source="${original}" 
+    alt="${description}" />
   </a>
 </li>`
 }).join('')
 }
 
+const cardsImages = createGalleryLayout(pictureFile);
+
+galleryListEl.insertAdjacentHTML('beforeend', cardsImages);
+
 galleryListEl.addEventListener('click', onOpenPictureByClick );
 
+//Открытие модального окна с большим изображением//
 function onOpenPictureByClick(e) {
-e.preventDefault();
+    e.preventDefault();
 
 if (!e.target.classList.contains('gallery__image')) {
 return;
 }
 
-modalEl.classList.add('is-open');
+    modalEl.classList.add('is-open');
 
-openModalBigImg.src = e.target.getAttribute('data-source');
+    openModalBigImg.src = e.target.getAttribute('data-source');
+     
+    btnEl.addEventListener('click', onClosePictureByClick);
+
+    backdropEl.addEventListener('click', onClosePictureByClick);
+
+    window.addEventListener('keydown', onClosePictureEsc);
+
+    
 }
 
+//Закрытие модального окна с большим изображением//
 function onClosePictureByClick() {
 
     openModalBigImg.removeAttribute('src');
 
     modalEl.classList.remove('is-open');
+
+    btnEl.removeEventListener('click', onClosePictureByClick);
+
+    backdropEl.removeEventListener('click', onClosePictureByClick);
+}
+
+//Закрытие модального окна с помощью  клавиши Esc //
+function onClosePictureEsc(e) {
+    if (e.code === 'Escape') {
+        onClosePictureByClick(e);
+        window.removeEventListener('keydown', onClosePictureEsc);
+    };
 }
